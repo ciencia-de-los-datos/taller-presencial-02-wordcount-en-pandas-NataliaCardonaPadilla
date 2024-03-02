13416562cf6e0d8d0e9da86318ed93d3bbc744e1
NataliaCardonaPadilla <104009930+NataliaCardonaPadilla@users.noreply.github.com>
2024-03-01 20:12:26 -0500

    Add files via upload

--- word_count.py
+++ word_count.py
@@ -4,6 +4,7 @@ import glob
 
 import pandas as pd
 
+
 def load_input(input_directory):
     """Load text files in 'input_directory/'"""
     #
@@ -11,29 +12,57 @@ def load_input(input_directory):
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
     # entrada en el DataFrame.
     #
-
+    filenames = glob.glob(f"{input_directory}/*.txt")
+    
+    #data_frames=[]
+    #for filename in filenames:
+    #    data_frames.append(pd.read_csv(filenames[0], sep="\t", header=None, names=["text"]))
+    data_frames=[
+        pd.read_csv(filename, sep="\t", header=None, names=["text"])
+        for filename in filenames
+    ]
+    
+    concatenated_df = pd.concat(data_frames, ignore_index=True)
+    
+    return concatenated_df
 
 def clean_text(dataframe):
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
-
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.lower()
+    dataframe["text"] = dataframe["text"].str.replace(".","")
+    dataframe["text"] = dataframe["text"].str.replace(",","")
+    return(dataframe)
 
 def count_words(dataframe):
     """Word count"""
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split()
+    dataframe = dataframe.explode("text")
+    dataframe = dataframe["text"].value_counts().reset_index()
+    #dataframe["count"] = 1
+    #dataframe = dataframe.groupby("text").agg({"count":"sum"})
+    # con value_counts omito la sulmas dos lineas comentadas, y esta función me ordena de mayor a menor
+    return dataframe
+
 
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
-
+    dataframe.to_csv(output_filename, sep="\t", index=False, header=False)
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def run(input_directory, output_filename):
     """Call all functions."""
-
+    df = load_input(input_directory)
+    df = clean_text(df)
+    df = count_words(df)
+    save_output(df, output_filename)
 
 if __name__ == "__main__":
     run(
